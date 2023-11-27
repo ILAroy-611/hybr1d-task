c2d60a5b6b1abe97838f6c21737a9925ae420ba7
ILAroy-611 <illaroy611@gmail.com>
2023-11-27 14:40:29 +0530

    Revert "post deployement change"
    This reverts commit aa922e2aaeaa20db9e6e2bfea5764f97de38f92b.

--- hybr1d-search/src/services/instance.js
+++ hybr1d-search/src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: `https://hn.algolia.com/api/v1`,
+    baseURL: `http://hn.algolia.com/api/v1`,
     headers: {
         'Content-Type': 'application/json',
     },
